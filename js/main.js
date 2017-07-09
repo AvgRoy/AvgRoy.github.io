7a532aec1fe7b434b49459218dfabba791de3dfc
Alexander Roy <alexander.roy96@gmail.com>
2017-07-08 20:39:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function main(){
   $('body > *:not(#border-top)').hide();
   $('body > *:not(#border)').hide();
-  $('.title').fadeIn(2500);
-  $('.icon1').fadeIn(2500);
-  $('.line').fadeIn(2500);
-  $('.about').fadeIn(5000);
-  $('.resume').fadeIn(5000);
+  $('.title').fadeIn(1250);
+  $('.icon1').fadeIn(1250);
+  $('.line').fadeIn(1250);
+  $('.about').fadeIn(2500);
+  $('.resume').fadeIn(2500);
 }
 $(document).ready(main);
